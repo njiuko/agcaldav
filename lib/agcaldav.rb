604f26a33580fa981ddcc31b43e965634dc4f99c
Patrick Schnetger <patrick.schnetger@gmail.com>
2016-08-11 14:59:28 +0200

    remove obsolte classes

--- lib/agcaldav.rb
+++ lib/agcaldav.rb
@@ -7,6 +7,6 @@ require 'icalendar'
 require 'time'
 require 'date'
 
-['errors/errors.rb','xml_request_builder.rb', 'client.rb', 'request.rb', 'net.rb', 'query.rb', 'filter.rb', 'format.rb'].each do |f|
+['errors/errors.rb','xml_request_builder.rb', 'client.rb', 'request.rb', 'net.rb', 'format.rb'].each do |f|
     require File.join( File.dirname(__FILE__), 'agcaldav', f )
 end
