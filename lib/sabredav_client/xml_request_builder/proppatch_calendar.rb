abf4df44600286e3db0b298f6097dd2df6ba541e
Nicolas Schwartau <n.schwartau@gmail.com>
2016-09-09 17:09:07 +0200

    adds possibility to update calendar access.
    This fit only possible if sabredav server uses the
    njiuko plugin

--- lib/sabredav_client/xml_request_builder/proppatch_calendar.rb
+++ lib/sabredav_client/xml_request_builder/proppatch_calendar.rb
@@ -2,20 +2,26 @@ module SabredavClient
   module XmlRequestBuilder
 
     class ProppatchCalendar < Base
-      attr_accessor :displayname, :description
+      attr_accessor :privilege, :displayname, :description
 
-      def initialize(displayname = nil, description = nil)
+      def initialize(displayname = nil, description = nil, privilege = nil)
         @displayname = displayname
         @description = description
+        @privilege   = privilege
         super()
       end
 
       def to_xml
-        xml.d :propertyupdate, C_NAMESPACES do
+        xml.d :propertyupdate, NAMESPACE.merge(C_NAMESPACES).merge(CS_NAMESPACES) do
           xml.d :set do
             xml.d :prop do
               xml.d :displayname, displayname unless displayname.nil?
               xml.tag! "c:calendar-description", description unless description.nil?
+              unless privilege.nil?
+                xml.cs :access do
+                  xml.tag! "cs:#{privilege}"
+                end
+              end
             end
           end
         end
